7b02be92e07a371eec805b6931ab74407cf91f32
GHILHANK <126936150+tegar-gilang@users.noreply.github.com>
2024-11-02 21:33:55 +0700

    Add files via upload

---
+++  07_Stack/GUIDED/guided.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+using namespace std;
+
+string arrayBuku[5];
+int maksimal = 5, top = 0;
+
+bool isFull() {
+    return (top == maksimal);
+}
+
+bool isEmpty() {
+    return (top == 0);
+}
+
+void pushArrayBuku(string data) {
+    if (isFull()) {
+        cout << "Data telah penuh" << endl;
+    } else {
+        arrayBuku[top] = data;
+        top++;
+    }
+}
+
+void popArrayBuku() {
+    if (isEmpty()) {
+        cout << "Tidak ada data yang dihapus" << endl;
+    } else {
+        arrayBuku[top - 1] = "";
+        top--;
+    }
+}
+
+void peekArrayBuku(int posisi) {
+    if (isEmpty()) {
+        cout << "Tidak ada data yang bisa dilihat" << endl;
+    } else {
+        int index = top;
+        for (int i = 1; i <= posisi; i++) {
+            index--;
+        }
+        cout << "Posisi ke " << posisi << " adalah " << arrayBuku[index] << endl;
+    }
+}
+
+int countStack() {
+    return top;
+}
+
+void changeArrayBuku(int posisi, string data) {
+    if (posisi > top) {
+        cout << "Posisi melebihi data yang ada" << endl;
+    } else {
+        int index = top;
+        for (int i = 1; i <= posisi; i++) {
+            index--;
+        }
+        arrayBuku[index] = data;
+    }
+}
+
+void destroyArraybuku() {
+    for (int i = top; i >= 0; i--) {
+        arrayBuku[i] = "";
+    }
+    top = 0;
+}
+
+void cetakArrayBuku() {
+    if (isEmpty()) {
+        cout << "Tidak ada data yang dicetak" << endl;
+    } else {
+        for (int i = top - 1; i >= 0; i--) {
+            cout << arrayBuku[i] << endl;
+        }
+    }
+}
+
+int main() {
+    pushArrayBuku("Kalkulus");
+    pushArrayBuku("Struktur Data");
+    pushArrayBuku("Matematika Diskrit");
+    pushArrayBuku("Dasar Multimedia");
+    pushArrayBuku("Inggris");
+
+    cetakArrayBuku();
+    cout << "\n";
+
+    cout << "Apakah data stack penuh? " << isFull() << endl;
+    cout << "Apakah data stack kosong? " << isEmpty() << endl;
+
+    peekArrayBuku(2);
+    popArrayBuku();
+
+    cout << "Banyaknya data = " << countStack() << endl;
+
+    changeArrayBuku(2, "Bahasa Jerman");
+    cetakArrayBuku();
+
+    cout << "\n";
+
+    destroyArraybuku();
+    cout << "Jumlah data setelah dihapus: " << top << endl;
+
+    cetakArrayBuku();
+
+    return 0;
+}
+
